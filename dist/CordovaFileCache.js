d034ac8da6854baaf8d449a34cac6f1d440e2b90
Mark Marijnissen <markmarijnissen@gmail.com>
2014-11-19 02:40:06 +0100

    build file

--- dist/CordovaFileCache.js
+++ dist/CordovaFileCache.js
@@ -256,10 +256,14 @@ var CordovaFileCache =
 	FileCache.prototype.toInternalURL = function toInternalURL(url){
 	  path = this.toPath(url);
 	  if(this._cached[path]) return this._cached[path].toInternalURL;
-	  return this.toServerURL(path);
+	  return this._fs.toInternalURLSync(path);
 	};
 
-	FileCache.prototype.get = FileCache.prototype.toInternalURL;
+	FileCache.prototype.get = function get(url){
+	  path = this.toPath(url);
+	  if(this._cached[path]) return this._cached[path].toInternalURL;
+	  return this.toServerURL(url);
+	};
 
 	FileCache.prototype.toDataURL = function toDataURL(url){
 	  return this._fs.toDataURL(this.toPath(url));
@@ -267,7 +271,7 @@ var CordovaFileCache =
 
 	FileCache.prototype.toURL = function toURL(url){
 	  path = this.toPath(url);
-	  return this._cached[path]? this._cached[path].toURL: this.toServerURL(url);
+	  return this._cached[path]? this._cached[path].toURL: url;
 	};
 
 	FileCache.prototype.toServerURL = function toServerURL(path){
