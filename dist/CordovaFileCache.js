7e46959b4be2a5a6d200a3b0e5177530963845e4
ashleybrener <ashley@starlogik.com>
2016-04-26 23:30:10 +0200

    Declare variable 'len'

--- dist/CordovaFileCache.js
+++ dist/CordovaFileCache.js
@@ -54,7 +54,7 @@ var CordovaFileCache =
 	  var self = this;
 	  // cordova-promise-fs
 	  this._fs = options.fs;
-	  if(!this._fs) { 
+	  if(!this._fs) {
 	    throw new Error('Missing required option "fs". Add an instance of cordova-promise-fs.');
 	  }
 	  // Use Promises from fs.
@@ -290,7 +290,7 @@ var CordovaFileCache =
 	      url = url.substr(0,query);
 	    }
 	    url = url = this._fs.normalize(url || '');
-	    len = this.serverRoot.length;
+	    var len = this.serverRoot.length;
 	    if(url.substr(0,len) !== this.serverRoot) {
 	      return this.localRoot + url;
 	    } else {
